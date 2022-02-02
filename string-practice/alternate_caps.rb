eed672ba4cbc9789427cba81356cb2d0d688b3d2
Gail Eddy <gaileddy68@gmail.com>
2022-02-02 13:20:49 -0600

    changed a few files

---
+++ string-practice/alternate_caps.rb
@@ -0,0 +1,14 @@
+# Given a string, write a function that returns a copy of the original string that has every other character capitalized. (Capitalization should begin with the second character.)
+
+#     Input: “hello, how are your porcupines today?”
+#     Output: “hElLo, HoW ArE YoUr pOrCuPiNeS ToDaY?”
+
+# 1. Write a function to input a string
+#  2. Iterate through the string 
+# 3. As your method iterates, put every two letters to a capital letter
+# 4. Increment by two as your iterate
+
+
+def alternate_caps(string)
+    
+end
