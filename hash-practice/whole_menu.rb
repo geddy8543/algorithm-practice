96ee770db89b627aed8a1a68f3265fc0bdf98efb
Gail Eddy <gaileddy68@gmail.com>
2022-02-14 12:20:26 -0600

    started menu code

---
+++ hash-practice/whole_menu.rb
@@ -0,0 +1,15 @@
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+# 1. define a menu with a hash that includes all of your key-value pairings
+# 2. Write a function that allows a user to place an order (they would input an array) User would input menu items
+# 3. 
+
+
+menu = {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
